39fde58f999f9822ca8c8eae92b2c45d0dda8862
Charlie Wilson <charles.ep.wilson@gmail.com>
2026-07-25 18:24:20 +0100

    win/lose sfx

---
+++ godot_project/game_board/undo_button.gd
@@ -0,0 +1,18 @@
+extends Button
+
+@onready var sfx: AudioStreamPlayer = $AudioStreamPlayer
+
+const speed_up: float = 4.0
+
+func _ready() -> void:
+	var pitch_effect: AudioEffect = AudioServer.get_bus_effect(
+		AudioServer.get_bus_index("SpeedUpPitch"),
+		0
+	) as AudioEffectPitchShift
+	pitch_effect.pitch_scale = 1 / speed_up
+	print(pitch_effect.pitch_scale)
+
+
+func _on_pressed():
+	sfx.pitch_scale = speed_up
+	sfx.play()
